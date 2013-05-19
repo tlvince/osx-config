c0b4b7bc0bf04e11f4f977ad0dfb1b30620880d3
Tom Vincent <git@tlvince.com>
2013-05-19 17:31:33 +0800

    Add update

--- aliases.sh
+++ aliases.sh
@@ -3,3 +3,8 @@ alias ls='ls -hG'
 alias o='open'
 alias oo='open .'
 alias oa='open -a'
+
+# Get OS X Software Updates, and update installed Ruby gems, Homebrew, npm, and
+# their installed packages
+alias update='sudo softwareupdate -ia; brew update; brew upgrade; \
+              brew cleanup; npm update npm -g; npm update -g; sudo gem update'
