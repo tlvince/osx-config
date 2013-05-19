92b26793a6e766b6769aede50cf27aa619c82d3d
Tom Vincent <git@tlvince.com>
2013-05-19 15:31:04 +0800

    Add OS X specific profile and shell bootstraps

---
+++ profile.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# http://stackoverflow.com/a/5084892
+export XDG_CONFIG_HOME="$HOME/Library/Application Support"
+export XDG_DATA_HOME="$HOME/Library"
+export XDG_CACHE_HOME="$HOME/Library/Caches"
