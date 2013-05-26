90c435540a735f328ca90d57f973654311c73370
Tom Vincent <git@tlvince.com>
2013-05-26 18:43:40 +0800

    Add /Users/tom/bin to path

--- exports.sh
+++ exports.sh
@@ -1,5 +1,5 @@
 # Brew
-export PATH="/usr/local/bin:/usr/local/sbin:/usr/local/share/python:/usr/local/share/npm/bin:$PATH"
+export PATH="$HOME/bin:/usr/local/bin:/usr/local/sbin:/usr/local/share/python:/usr/local/share/npm/bin:$PATH"
 
 # MacPorts
 export PATH="/opt/local/bin:/opt/local/sbin:$PATH"
