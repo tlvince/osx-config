005870bb3d0feda85a5076e99b6d31900f6b6c3f
Tom Vincent <git@tlvince.com>
2013-06-29 13:06:08 +0800

    Python no longer needed in PATH, add Android

--- exports.sh
+++ exports.sh
@@ -1,5 +1,8 @@
 # Brew
-export PATH="$HOME/bin:/usr/local/bin:/usr/local/sbin:/usr/local/share/python:/usr/local/share/npm/bin:$PATH"
+export PATH="$HOME/bin:/usr/local/bin:/usr/local/sbin:/usr/local/share/npm/bin:$PATH"
 
 # MacPorts
 export PATH="/opt/local/bin:/opt/local/sbin:$PATH"
+
+# Android (brew)
+export ANDROID_SDK_ROOT="/usr/local/opt/android-sdk"
