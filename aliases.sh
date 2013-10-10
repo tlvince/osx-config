59bf139eb55c4cc7dbcf5bad1f677c7ea7da56e1
Tom Vincent <git@tlvince.com>
2013-10-10 19:55:49 +0100

    Move update into script for scalability

--- aliases.sh
+++ aliases.sh
@@ -3,8 +3,3 @@ alias ls='ls -hG'
 alias o='open'
 alias oo='open .'
 alias oa='open -a'
-
-# Get OS X Software Updates, and update installed Ruby gems, Homebrew, npm, and
-# their installed packages
-alias update='sudo softwareupdate -ia; brew update; brew upgrade; \
-              brew cleanup; npm update npm -g; npm update -g; sudo gem update'
