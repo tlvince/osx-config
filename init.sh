25bfeae7218082390bc3bc63195f3f28d895f801
Tom Vincent <git@tlvince.com>
2013-10-27 20:21:54 +0000

    Add mutt init

--- init.sh
+++ init.sh
@@ -7,3 +7,9 @@ _have "brew" || ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
 brew update
 brew install git zsh tree htop-osx tmux hub
 brew install vim --disable-nls --without-ruby
+
+brew tap tlvince/homebrew-tlvince
+brew install tlvince/tlvince/mutt --HEAD --mua \
+  --with-confirm-attachment-patch \
+  --with-confirm-crypt-hook-patch \
+  --with-pgp-verbose-mime-patch
