9c1ae64ec793e6294fa0983d601b6d28bfb8c84c
Tom Vincent <git@tlvince.com>
2013-10-27 17:11:28 +0000

    Localise, rm XDG setup

---
+++ .osx/profile.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+source "$HOME/.osx/aliases.sh"
+source "$HOME/.osx/imports.sh"
+source "$HOME/.osx/exports.sh"
--- shell.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-source "$XDG_CONFIG_HOME/osx/aliases.sh"
-source "$XDG_CONFIG_HOME/osx/imports.sh"
-source "$XDG_CONFIG_HOME/osx/exports.sh"
