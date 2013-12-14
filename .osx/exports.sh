674741c8ccc418de59fed9050618364efdc87e1a
Tom Vincent <git@tlvince.com>
2013-12-14 13:29:05 +0000

    Not using MacPorts, add KDE

--- .osx/exports.sh
+++ .osx/exports.sh
@@ -1,8 +1,9 @@
 # Brew
 export PATH="$HOME/bin:/usr/local/bin:/usr/local/sbin:/usr/local/share/npm/bin:$PATH"
 
-# MacPorts
-export PATH="/opt/local/bin:/opt/local/sbin:$PATH"
-
 # Android (brew)
 export ANDROID_SDK_ROOT="/usr/local/opt/android-sdk"
+
+# KDE (brew)
+export KDEDIR="/usr/local"
+export KDEDIRS="$KDEDIRS:$HOME/Library/Preferences/KDE:$KDEDIR"
