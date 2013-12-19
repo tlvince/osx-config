a6db3388e66891190ba2d41b42dcec61c3bf3753
Tom Vincent <git@tlvince.com>
2013-12-19 15:14:52 +0000

    Add BasicTeX to PATH

--- .osx/exports.sh
+++ .osx/exports.sh
@@ -1,9 +1,15 @@
 # Brew
-export PATH="$HOME/bin:/usr/local/bin:/usr/local/sbin:/usr/local/share/npm/bin:$PATH"
+export PATH="$HOME/bin:/usr/local/bin:/usr/local/sbin:$PATH"
 
-# Android (brew)
+# Node (brewed)
+export PATH="/usr/local/share/npm/bin:$PATH"
+
+# BasicTeX (casked)
+export PATH="/usr/local/texlive/2013basic/bin/x86_64-darwin:$PATH"
+
+# Android (brewed)
 export ANDROID_SDK_ROOT="/usr/local/opt/android-sdk"
 
-# KDE (brew)
+# KDE (brewed)
 export KDEDIR="/usr/local"
 export KDEDIRS="$KDEDIRS:$HOME/Library/Preferences/KDE:$KDEDIR"
