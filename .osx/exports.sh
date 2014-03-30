f680d761208d058c5c38b94352c75dad104d07ff
Tom Vincent <git@tlvince.com>
2014-03-30 11:22:34 +0100

    Maintain PATH priority

--- .osx/exports.sh
+++ .osx/exports.sh
@@ -2,10 +2,10 @@
 export PATH="$HOME/bin:/usr/local/bin:/usr/local/sbin:$PATH"
 
 # Node (brewed)
-export PATH="/usr/local/share/npm/bin:$PATH"
+export PATH="$PATH:/usr/local/share/npm/bin"
 
 # BasicTeX (casked)
-export PATH="/usr/local/texlive/2013basic/bin/x86_64-darwin:$PATH"
+export PATH="$PATH:/usr/local/texlive/2013basic/bin/x86_64-darwin"
 
 # Android (brewed)
 export ANDROID_SDK_ROOT="/usr/local/opt/android-sdk"
