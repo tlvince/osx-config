7a16a7d4a6d5d34d8ec31425fbe168a4faf3a194
Tom Vincent <git@tlvince.com>
2014-10-24 17:52:44 +0100

    Drop all non-brew PATHs

--- .osx/exports.sh
+++ .osx/exports.sh
@@ -3,24 +3,3 @@ export PATH="$HOME/bin:/usr/local/bin:/usr/local/sbin:$PATH"
 
 # Node (brewed)
 export PATH="$PATH:/usr/local/share/npm/bin"
-
-# BasicTeX (casked)
-export PATH="$PATH:/usr/texbin"
-
-# gcc-arm
-export PATH="$PATH:/opt/gcc-arm/bin"
-
-# Macports
-export PATH="$PATH:/opt/local/bin"
-
-# Android (brewed)
-export ANDROID_SDK_ROOT="/usr/local/opt/android-sdk"
-export ANDROID_HOME="$ANDROID_SDK_ROOT"
-
-# KDE (brewed)
-export KDEDIR="/usr/local"
-export KDEDIRS="$KDEDIRS:$HOME/Library/Preferences/KDE:$KDEDIR"
-
-# Karma tests
-export CHROME_BIN="/opt/chromium/chrome-mac/Chromium.app/Contents/MacOS/Chromium"
-export FIREFOX_BIN="/opt/homebrew-cask/Caskroom/firefox/latest/Firefox.app/Contents/MacOS/firefox"
