42d7931446e4c2749928565f2d1f88f0709fabe1
Tom Vincent <git@tlvince.com>
2015-05-04 22:31:58 +0200

    fix(path): restore opt bins

--- .osx/exports.sh
+++ .osx/exports.sh
@@ -7,5 +7,8 @@ export PATH="$PATH:/usr/local/share/npm/bin"
 # Android SDK (brewed)
 export ANDROID_HOME="/usr/local/opt/android-sdk"
 
+# System
+export PATH="$PATH:/opt/local/bin"
+
 # Local
 export PATH="$HOME/bin:$PATH"
