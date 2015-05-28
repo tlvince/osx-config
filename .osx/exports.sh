a8adc85c198390af4f50ced0dd30a79b22b93f5e
Tom Vincent <git@tlvince.com>
2015-05-28 15:26:44 +0100

    fix(exports): add Karma support

--- .osx/exports.sh
+++ .osx/exports.sh
@@ -10,5 +10,9 @@ export ANDROID_HOME="/usr/local/opt/android-sdk"
 # System
 export PATH="$PATH:/opt/local/bin"
 
+# Karma
+export CHROME_BIN="/opt/homebrew-cask/Caskroom/chromium/latest/chrome-mac/Chromium.app/Contents/MacOS/Chromium"
+export FIREFOX_BIN="/opt/homebrew-cask/Caskroom/firefox/latest/Firefox.app/Contents/MacOS/firefox"
+
 # Local
 export PATH="$HOME/bin:$PATH"
