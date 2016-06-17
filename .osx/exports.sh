ed2b7e580fa6e18db4b7250dcef10e557c6d36cb
Tom Vincent <git@tlvince.com>
2016-06-17 11:45:25 +0200

    feat(exports): add python pip

--- .osx/exports.sh
+++ .osx/exports.sh
@@ -14,5 +14,9 @@ export PATH="$PATH:/opt/local/bin"
 export CHROME_BIN="/opt/homebrew-cask/Caskroom/chromium/latest/chrome-mac/Chromium.app/Contents/MacOS/Chromium"
 export FIREFOX_BIN="/opt/homebrew-cask/Caskroom/firefox/latest/Firefox.app/Contents/MacOS/firefox"
 
+# Pip
+export PATH="$PATH:$HOME/Library/Python/2.7/bin"
+export PATH="$PATH:$HOME/Library/Python/3.5/bin"
+
 # Local
 export PATH="$HOME/bin:$PATH"
