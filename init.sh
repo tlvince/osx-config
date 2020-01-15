01344aa3d64068449656bbc3427d04455cd194f7
Tom Vincent <tom.vincent@clearscore.com>
2020-01-15 11:09:59 +0000

    fix(init): remove opts handling, mutt, cask

--- init.sh
+++ init.sh
@@ -5,24 +5,11 @@ _have() { command -v "$1" >/dev/null; }
 _have "brew" || ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 BREWS=".brew"
-# Iterate so options are preserved
-[ -f "$BREWS" ] && {
-  while read -r formula; do
-    brew install $formula
-  done < "$BREWS"
-}
-
-_have "mutt" || {
-  brew tap tlvince/homebrew-tlvince
-  brew install tlvince/tlvince/mutt --mua \
-    --with-confirm-attachment-patch \
-    --with-confirm-crypt-hook-patch \
-    --with-pgp-verbose-mime-patch
-}
+[ -f "$BREWS" ] && brew install $(<"$BREWS")
 
 _have "mpv" || {
   brew tap mpv-player/mpv
-  brew install ffmpeg --with-openssl
+  brew install ffmpeg
   brew install --HEAD mpv-player/mpv/libass-ct
   brew install --HEAD mpv
 }
@@ -30,11 +17,6 @@ _have "mpv" || {
 _have "get_iplayer" || {
   brew tap dinkypumpkin/get_iplayer
   brew install rtmpdump atomicparsley id3v2 lame get_iplayer
-}
-
-brew cask >/dev/null 2>&1 || {
-  brew install caskroom/cask/brew-cask
-  brew tap caskroom/versions
 }
 
 CASKS=".cask"
